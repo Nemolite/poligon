e2a22f281fbdb38453960b79fd3377e005c3feee
Nemolite <g25092011@mail.ru>
2017-10-21 21:50:39 +0300

    interface

--- index.php
+++ index.php
@@ -1,59 +1,55 @@
 <?php
 //phpinfo();
-define('__ROOT__',$_SERVER['DOCUMENT_ROOT']); 
 
-require_once(__ROOT__.'/Test.php'); 
-require_once(__ROOT__.'/stat.php'); 
-
-$obj = new Test; 
-$obj->set(20);
-$obj->show();
-//var_dump(parse_url($url));
-
-
-$message = 'hello';
-
-// Без "use"
-$example = function () {
-    var_dump($message);
-};
-echo "<br>";
-$example();
-echo "<br>";
+interface I 
+{ 
+     public function f(); 
+}
+class C implements I 
+{ 
+		public function f() {
+			
+		} 
+}
 
 
+class E 
+{
+	
+}
 
-// Наследуем $message
-$example = function () use ($message) {
-    var_dump($message);
-};
-$example();
-echo "<br>";
-// Значение унаследованной переменной задано там, где функция определена, 
-// но не там, где вызвана
-$message = 'world';
-$example();
-echo "<br>";
-// Сбросим message
-$message = 'hello';
+function f(I $i) {
+    echo get_class($i)."\n";
+}
 
-// Наследование по ссылке
-$example = function () use (&$message) {
-    var_dump($message);
-};
-$example();
-echo "<br>";
-// Измененное в родительской области видимости значение
-// остается тем же внутри вызова функции
-$message = 'world';
-echo $example();
+f(new C);
+f(new E);
 
-// Замыкания могут принимать обычные аргументы
-$example = function ($arg) use ($message) {
-    var_dump($arg . ' ' . $message);
-};
-$example("hello");
-echo "<br>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//$text = 'аоут';
+
+//$regular_expression ='/^[аоие\ёэыуюя].*$/';
+
+//if (preg_match($regular_expression,$text)){
+//	echo "ok";
+//} else {
+//	echo "no";
+//}
+
+
 ?>
 
 
